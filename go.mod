4d1692db9b663ac0b37ffa212ef238f1184bec9d
alexey-y-a <lexus2157@mail.ru>
2025-08-03 01:00:56 +0300

    step_001

---
+++ go.mod
@@ -0,0 +1,3 @@
+module jcloud
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import "time"
+
+type Bin struct {
+	ID        string    `json:"id"`
+	Private   bool      `json:"private"`
+	CreatedAt time.Time `json:"createdAt"`
+	Name      string    `json:"name"`
+}
+
+type BinList struct {
+	Bins []Bin `json:"bins"`
+}
+
+func NewBin(id string, private bool, name string) Bin {
+	return Bin{
+		ID:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+}
+
+func NewBinList() BinList {
+	return BinList{
+		Bins: make([]Bin, 0),
+	}
+}
+
+func main() {
+
+}
