2ceabe696bd0246ef37e27b4f06606aaa3f7ea4d
alexey-y-a <lexus2157@mail.ru>
2025-08-07 21:39:33 +0300

    step_005

--- api/api.go
+++ api/api.go
@@ -1,18 +1,103 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"jcloud/bins"
 	"jcloud/config"
+	"jcloud/files"
+	"jcloud/storage"
+	"net/http"
+)
+
+const (
+	baseURL = "https://api.jsonbin.io/v3/b"
 )
 
 type API struct {
-	cfg *config.Config
+	cfg     *config.Config
+	storage storage.Storage
 }
 
-func New(cfg *config.Config) *API {
-	return &API{cfg: cfg}
+func New(cfg *config.Config, storage storage.Storage) *API {
+	return &API{
+		cfg:     cfg,
+		storage: storage,
+	}
 }
 
-func (a *API) MakeRequest() {
-	fmt.Printf("Making request with API key: %s\n", a.cfg.APIKey)
+func (a *API) makeRequest(method, path string, body interface{}) ([]byte, error) {
+	var reqBody []byte
+	if body != nil {
+		var err error
+		reqBody, err = json.Marshal(body)
+		if err != nil {
+			return nil, fmt.Errorf("error marshaling request body: %v", err)
+		}
+	}
+
+	req, err := http.NewRequest(method, baseURL+path, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Master-Key", a.cfg.APIKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("API request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %v", err)
+	}
+
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("API error: %s", string(respBody))
+	}
+
+	return respBody, nil
+}
+
+func (a *API) CreateBin(filePath, name string) (*bins.Bin, error) {
+	fs := files.FileSystem{}
+	data, err := fs.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %v", err)
+	}
+
+	var content interface{}
+	if err := json.Unmarshal(data, &content); err != nil {
+		return nil, fmt.Errorf("file must contain valid JSON: %v", err)
+	}
+
+	resp, err := a.makeRequest("POST", "", map[string]interface{}{
+		"name":    name,
+		"private": true,
+		"data":    content,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var result struct {
+		Metadata bins.Bin `json:"metadata"`
+	}
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return nil, fmt.Errorf("error decoding response: %v", err)
+	}
+
+	binList, _ := a.storage.LoadBins("bins.json")
+	binList.Bins = append(binList.Bins, result.Metadata)
+	if err := a.storage.SaveBins(binList, "bins.json"); err != nil {
+		return nil, fmt.Errorf("error saving bin: %v", err)
+	}
+
+	return &result.Metadata, nil
 }
--- main.go
+++ main.go
@@ -1,37 +1,77 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"jcloud/api"
-	"jcloud/bins"
 	"jcloud/config"
 	"jcloud/files"
 	"jcloud/storage"
+	"os"
 )
 
 func main() {
+	create := flag.Bool("create", false, "Create new bin")
+	update := flag.Bool("update", false, "Update existing bin")
+	delete := flag.Bool("delete", false, "Delete bin")
+	get := flag.Bool("get", false, "Get bin content")
+	list := flag.Bool("list", false, "List all bins")
+	file := flag.String("file", "", "Path to JSON file")
+	name := flag.String("name", "", "Bin name")
+	binID := flag.String("id", "", "Bin ID")
+	// verbose := flag.Bool("v", false, "Verbose output")
+
+	flag.Parse()
+
 	cfg := config.Load()
-
-	apiClient := api.New(cfg)
-	apiClient.MakeRequest()
-
 	fileSystem := files.FileSystem{}
 	storage := storage.NewJsonStorage(fileSystem)
+	apiClient := api.New(cfg, storage)
 
-	binList := bins.NewBinList()
-	binList.Bins = append(binList.Bins, bins.NewBin("1", false, "Test Bin"))
+	switch {
+	case *create:
+		if *file == "" || *name == "" {
+			fmt.Println("Error: both --file and --name are required for create")
+			os.Exit(1)
+		}
+		bin, err := apiClient.CreateBin(*file, *name)
+		if err != nil {
+			fmt.Printf("Error creating bin: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Created bin: ID=%s, Name=%s\n", bin.ID, bin.Name)
 
-	err := storage.SaveBins(binList, "bins.json")
-	if err != nil {
-		fmt.Printf("Error saving bins: %v\n", err)
-		return
+	case *update:
+		if *file == "" || *binID == "" {
+			fmt.Println("Error: both --file and --id are required for update")
+			os.Exit(1)
+		}
+		fmt.Println("Update command not implemented yet")
+		os.Exit(0)
+
+	case *delete:
+		if *binID == "" {
+			fmt.Println("Error: --id is required for delete")
+			os.Exit(1)
+		}
+		fmt.Println("Delete command not implemented yet")
+		os.Exit(0)
+
+	case *get:
+		if *binID == "" {
+			fmt.Println("Error: --id is required for get")
+			os.Exit(1)
+		}
+		fmt.Println("Get command not implemented yet")
+		os.Exit(0)
+
+	case *list:
+		fmt.Println("List command not implemented yet")
+		os.Exit(0)
+
+	default:
+		fmt.Println("No valid command specified")
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-
-	loadedBins, err := storage.LoadBins("bins.json")
-	if err != nil {
-		fmt.Printf("Error loading bins: %v\n", err)
-		return
-	}
-
-	fmt.Printf("Loaded bins: %+v\n", loadedBins)
 }
